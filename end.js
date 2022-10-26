6dc27f59a1b50c3312603850694c0dbdd06f2a42
Ella Bryning <ellabryning@gmail.com>
2022-10-27 09:05:33 +1100

    end page stuff

--- end.js
+++ end.js
@@ -2,7 +2,7 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('finalScore');
-
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -13,4 +13,12 @@ username.addEventListener('keyup', () => {
 function saveHighScore (e) {
   e.preventDefault();
   console.log('clicked save');
-}
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value
+  };
+
+  highScores.push(score);
+  console.log(highScores);
+};
